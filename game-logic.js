3bd96f3287391297349a216b973fbc657749a6c2
emesefedev <emesefedev@gmail.com>
2024-07-24 16:46:15 +0200

    play round

--- game-logic.js
+++ game-logic.js
@@ -5,20 +5,98 @@ const choices = {
 }
 
 let humanScore = 0
-let comuterScore = 0
+let computerScore = 0
+
+game()
+
+function game() {
+    const humanChoice = getHumanChoice()
+    const computerChoice = getComputerChoice()
+
+    playRound(humanChoice, computerChoice)
+
+    console.log(`H: ${humanScore}, C: ${computerScore}`)
+}
+
+function playRound(humanChoice, computerChoice) {
+    let message = ""
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "paper":
+                    computerScore++
+                    message = `You lose! ${computerChoice} beats ${humanChoice} :(`
+                    break
+                case "scissors":
+                    humanScore++
+                    message = `You win! ${humanChoice} beats ${computerChoice} :)`
+                    break
+                default:
+                    message = `Draw! :|`
+                    break
+            }
+            break
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    humanScore++
+                    message = `You win! ${humanChoice} beats ${computerChoice} :)`
+                    break
+                case "scissors":
+                    computerScore++
+                    message = `You lose! ${computerChoice} beats ${humanChoice} :(`
+                    break
+                default:
+                    message = `Draw! :|`
+                    break
+            }
+            break
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    computerScore++
+                    message = `You lose! ${computerChoice} beats ${humanChoice} :(`
+                    break
+                case "paper":
+                    humanScore++
+                    message = `You win! ${humanChoice} beats ${computerChoice} :)`
+                    break
+                default:
+                    message = `Draw! :|`
+                    break
+            }
+            break     
+    }
+    console.log(message)
+}
 
 function getHumanChoice() {
+
+    let isValidChoice = false
+    let message = "Write your option: rock, paper, scissors"
+    let humanChoice = null
+
     do {
-        const choice = prompt("Write your option: rock, paper, scissors")
-        switch (choice) {
+        const choice = prompt(message)
+        switch (choice.toLocaleLowerCase()) {
             case "rock":
-                return choices.rock
+                humanChoice = choices.rock
+                isValidChoice = true
+                break
             case "paper":
-                return choices.paper
+                humanChoice = choices.paper
+                isValidChoice = true
+                break
             case "scissors":
-                return choices.scissors
+                humanChoice = choices.scissors
+                isValidChoice = true
+                break
+            default:
+                message = "WRONG ANSWER! Try again: rock, paper, scissors"
         } 
-    } while (true);
+    } while (!isValidChoice)
+
+    return humanChoice
     
 }
 

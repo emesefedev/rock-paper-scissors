b8071d9a3b4574931fe32d37064026ae1c53b791
emesefedev <emesefedev@gmail.com>
2024-07-24 16:13:29 +0200

    write computer choice

--- game-logic.js
+++ game-logic.js
@@ -1,1 +1,17 @@
-console.log("Rock, paper, scissors")
+const choices = {
+    rock: "rock",
+    paper: "paper",
+    scissors: "scissors"
+}
+
+getComputerChoice()
+
+function getComputerChoice() {
+    const randomChoice = getRandomInt(Object.keys(choices).length)
+    console.log(randomChoice)
+    console.log(Object.keys(choices)[randomChoice])
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
